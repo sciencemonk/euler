0f14c1eda0d1a1dbdc6a5b2a7242dcef27f2b4af
Michael Olaya <michaelaolaya@gmail.com>
2018-01-12 22:12:34 -0600

    Update 2.py

--- Solutions/2.py
+++ Solutions/2.py
@@ -4,7 +4,6 @@
 #By considering the terms in the Fibonacci sequence whose values do not exceed
 #four million, find the sum of the even-valued terms.
 
-
 def fib(upper):
     previous = 1
     current = 2
